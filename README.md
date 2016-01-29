060193d1d4abcdfccc72e9c9d752316c677a4da1
futurepneu <daniel@fre-e-motion.de>
2016-01-29 20:48:15 +0100

    Made some changes to g_weapon.cpp: -fix: make grenades explode on doors and buttons -fix: changed grenade angle to racesow default -fix: changed grenade bounce count to racesow default

--- README.md
+++ README.md
@@ -3,7 +3,12 @@ Source modification of the "warsow_201_sdk". - Let's try to add as many racesow 
 
 # Changelog:
 29.01.2016:
+g_func.cpp
  * defrag support: do not trigger if the door was killed
  * fix for "doorbuttons" trigger targets without opening
  * fix for wait key on buttons: use -1 to reset immediately
  * fix: all buttons without health should be touchable regardless of targetname
+g_weapon.cpp
+ * fix: make grenades explode on doors and buttons
+ * fix: changed grenade angle to racesow default
+ * fix: changed grenade bounce count to racesow default
--- source/game/g_weapon.cpp
+++ source/game/g_weapon.cpp
@@ -676,7 +676,8 @@ static void W_Touch_Grenade( edict_t *ent, edict_t *other, cplane_t *plane, int 
 		return;
 
 	// don't explode on doors and plats that take damage
-	if( !other->takedamage || ISBRUSHMODEL( other->s.modelindex ) )
+	// racesow - remove check || ISBRUSHMODEL( other->s.modelindex )
+	if( !other->takedamage )
 	{
 		G_AddEvent( ent, EV_GRENADE_BOUNCE, ( ent->s.effects & EF_STRONG_WEAPON ) ? FIRE_MODE_STRONG : FIRE_MODE_WEAK, true );
 		return;
@@ -726,7 +727,8 @@ edict_t *W_Fire_Grenade( edict_t *self, vec3_t start, vec3_t angles, int speed, 
 
 	if( aim_up )
 	{
-		angles[PITCH] -= 5; // aim some degrees upwards from view dir
+		if( !GS_RaceGametype() ) // racesow
+			angles[PITCH] -= 10; // aim some degrees upwards from view dir
 
 		// clamp to front side of the player
 		angles[PITCH] += -90; // rotate to make easier the check
@@ -751,6 +753,7 @@ edict_t *W_Fire_Grenade( edict_t *self, vec3_t start, vec3_t angles, int speed, 
 	{
 		grenade->s.modelindex = trap_ModelIndex( PATH_GRENADE_STRONG_MODEL );
 		grenade->s.effects |= EF_STRONG_WEAPON;
+		grenade->health = 2; // racesow - bounce count
 	}
 	else
 	{
